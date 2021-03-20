18fd400166ba953e09d00c0ba4cf48fe5ed05407
davidhenshaw <davidhenshaw2014@gmail.com>
2021-03-20 09:29:17 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainElement = document.querySelector("main#main");
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'David is the champion';
+
+document.querySelector("body").appendChild(newHeader);
